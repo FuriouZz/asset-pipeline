cddb476b0e2f2a485912936b461f66119971db7a
Christophe Massolin <christophe.massolin@gmail.com>
2021-08-06 00:23:05 +0200

    Rules can accept function

--- lib/transform-rule.ts
+++ lib/transform-rule.ts
@@ -1,141 +1,178 @@
-import { createRule } from "./rule"
+import { createRule } from "./rule";
 import { basename, dirname, format, join, parse, relative } from "path";
 import { generateHash } from "./utils";
 import { normalize } from "./path/utils";
 import { RuleOptions, TransformedPath } from "./types";
 
+type AcceptedInput = string | number | boolean;
+type InputFunction<T extends AcceptedInput> = (
+  filename: string,
+  options?: Partial<RuleOptions>
+) => T;
+type Input<T extends AcceptedInput> = T | InputFunction<T>;
+
 export interface TransformRuleOptions {
-  name?: string
-  extension?: string
-  directory?: string
-  baseDirectory?: string
-  relative?: string
-  cachebreak: boolean
+  name?: Input<string>;
+  extension?: Input<string>;
+  directory?: Input<string>;
+  baseDirectory?: Input<string>;
+  relative?: Input<string>;
+  cachebreak?: Input<boolean>;
+  pre: (["path", Input<string>] | ["keepDirectory", Input<boolean>])[];
 }
 
-export type TransformRule = ReturnType<typeof CreateTransformRule>
+export type TransformRule = ReturnType<typeof CreateTransformRule>;
 
-const EXT_REG = /^\./
+const EXT_REG = /^\./;
 
 export const CreateTransformRule = createRule({
   options() {
     return <TransformRuleOptions>{
       tag: "default",
-      cachebreak: true
-    }
+      cachebreak: true,
+      pre: [],
+    };
   },
 
   methods: {
-
-    name(name: string) {
-      this.options.name = name
-      return this
+    name(name?: Input<string>) {
+      this.options.name = name;
+      return this;
     },
 
-    directory(directory: string) {
-      this.options.directory = directory
-      return this
+    directory(directory?: Input<string>) {
+      this.options.directory = directory;
+      return this;
     },
 
-    baseDirectory(baseDirectory: string) {
-      this.options.baseDirectory = baseDirectory
-      return this
+    baseDirectory(baseDirectory?: Input<string>) {
+      this.options.baseDirectory = baseDirectory;
+      return this;
     },
 
-    relative(relative: string) {
-      this.options.relative = relative
-      return this
+    relative(relative?: Input<string>) {
+      this.options.relative = relative;
+      return this;
     },
 
-    cachebreak(enable: boolean) {
-      this.options.cachebreak = enable
-      return this
+    cachebreak(enable?: Input<boolean>) {
+      this.options.cachebreak = enable;
+      return this;
     },
 
-    path(path: string) {
-      this.options.directory = dirname(path)
-      const parts = basename(path).split(".")
-      const name = parts.shift()
-      const extension = parts.join(".")
-      if (name) this.options.name = name
-      if (extension) this.extension(`.${extension}`)
-      return this
+    path(path: Input<string>) {
+      this.options.pre.push(["path", path]);
+      return this;
     },
 
-    extension(extension: string) {
-      if (!EXT_REG.test(extension)) {
-        extension = `.${extension}`
-      }
-      this.options.extension = extension
-      return this
+    extension(extension?: Input<string>) {
+      this.options.extension = extension;
+      return this;
     },
 
-    keepDirectory(enable: boolean) {
-      if (enable) {
-        delete this.options.directory
-      } else {
-        this.options.directory = "."
-      }
-      return this
+    keepDirectory(enable: Input<boolean>) {
+      this.options.pre.push(["keepDirectory", enable]);
+      return this;
     },
 
     apply(filename: string, options?: Partial<RuleOptions>): TransformedPath {
       if (!this.match(filename)) {
-        throw new Error(`Cannot tranform "${filename}"`)
+        throw new Error(`Cannot tranform "${filename}"`);
       }
 
       const _options: RuleOptions = {
         cachebreak: false,
         saltKey: "none",
-        ...(options || {})
-      }
+        ...(options || {}),
+      };
 
-      const rule = this.options
-      let output = filename
+      const get = <T extends AcceptedInput>(entry: Input<T>) => {
+        if (typeof entry === "function") {
+          return entry(filename, options);
+        }
 
-      const hash = generateHash(output + _options.saltKey)
-      const parsed = parse(output)
+        return entry;
+      };
+
+      const rule = { ...this.options };
+
+      rule.pre.forEach((pre) => {
+        switch (pre[0]) {
+          case "path": {
+            const path = get(pre[1]);
+            rule.directory = dirname(path);
+            const parts = basename(path).split(".");
+            const name = parts.shift();
+            const extension = parts.join(".");
+            if (name) rule.name = name;
+            if (extension && !rule.extension) rule.extension = `.${extension}`;
+            break;
+          }
+
+          case "keepDirectory": {
+            const enable = get(pre[1]);
+            if (enable) {
+              delete rule.directory;
+            } else {
+              rule.directory = ".";
+            }
+            break;
+          }
+        }
+      });
+
+      let output = filename;
+
+      const hash = generateHash(output + _options.saltKey);
+      const parsed = parse(output);
 
       // Fix ext and name
-      const parts = parsed.base.split(".")
-      const name = parts.shift()!
-      parsed.name = name
-      parsed.ext = `.${parts.join(".")}`
+      const parts = parsed.base.split(".");
+      const name = parts.shift()!;
+      parsed.name = name;
+      parsed.ext = `.${parts.join(".")}`;
 
-      if (typeof rule.directory === "string" && rule.directory) {
-        parsed.dir = rule.directory
+      if (typeof rule.directory !== "undefined") {
+        parsed.dir = get(rule.directory);
       }
 
-      if (typeof rule.relative === "string" && rule.relative) {
-        parsed.dir = relative(rule.relative, parsed.dir)
+      if (typeof rule.relative !== "undefined") {
+        parsed.dir = relative(get(rule.relative), parsed.dir);
       }
 
-      if (typeof rule.baseDirectory === "string" && rule.baseDirectory) {
-        parsed.dir = join(rule.baseDirectory, parsed.dir)
+      if (typeof rule.baseDirectory !== "undefined") {
+        parsed.dir = join(get(rule.baseDirectory), parsed.dir);
       }
 
-      if (typeof rule.name === "string" && rule.name) {
-        parsed.name = rule.name
+      if (typeof rule.name !== "undefined") {
+        parsed.name = get(rule.name);
       }
 
-      if (_options.cachebreak && rule.cachebreak) {
-        parsed.name = `${parsed.name}-${hash}`
+      if (_options.cachebreak && typeof rule.cachebreak !== "undefined") {
+        if (get(rule.cachebreak)) {
+          parsed.name = `${parsed.name}-${hash}`;
+        }
       }
 
-      if (typeof rule.extension === "string" && rule.extension) {
-        parsed.ext = rule.extension
+      if (typeof rule.extension !== "undefined") {
+        let extension = get(rule.extension);
+
+        if (!EXT_REG.test(extension)) {
+          extension = `.${extension}`;
+        }
+
+        parsed.ext = extension;
       }
 
-      parsed.base = parsed.name + parsed.ext
+      parsed.base = parsed.name + parsed.ext;
 
-      output = format(parsed)
+      output = format(parsed);
 
       return {
         path: normalize(output, "web"),
         tag: rule.tag,
-        priority: rule.priority
-      }
-    }
-
-  }
-})
+        priority: rule.priority,
+      };
+    },
+  },
+});
